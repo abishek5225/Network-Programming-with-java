57f3d3722d9df5bdf01e8c756f8532df6375b0c0
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-10 18:41:46 +0545

    updated

---
+++ src/Networking/Client.java
@@ -0,0 +1,15 @@
+package Networking;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        try{
+            Socket socket=new Socket("localhost",5000);
+
+            //read message from server
+
+        }
+    }
+}
