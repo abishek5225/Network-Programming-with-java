5ac9485592158b1f71ccaedf1c6156d0d67943f2
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-10 18:25:55 +0545

    updated

--- src/Networking/Server.java
+++ src/Networking/Server.java
@@ -6,7 +6,13 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) {
         try{
+    ServerSocket serverSocket=new ServerSocket(5000);
+            System.out.println("Server is running");
 
+            Socket socket = serverSocket.accept();
+            System.out.println("client connected");
+
+            Output
         }
     }
 }
