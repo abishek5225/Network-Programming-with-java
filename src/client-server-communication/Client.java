24a00b2b9074b10cff8eae1c50a470b9511e3c87
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-11 23:11:19 +0545

    updated

--- src/client-server-communication/Client.java
+++ src/client-server-communication/Client.java
@@ -6,6 +6,14 @@ public class Client {
         try{
             Socket socket=new Socket("localhost",3000);
             System.out.println("Connected to server");
+
+            BufferedReader input= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output= new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader consoleInput=new BufferedReader(new InputStreamReader(System.in));
+
+            String serverMessage, clientReply;
+
         }catch (Exception e){
             e.printStackTrace();
         }
