decbda0655cece691840497a471097e1cc686ea1
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-11 22:18:21 +0545

    updated

--- src/client-server-communication/Server.java
+++ src/client-server-communication/Server.java
@@ -6,36 +6,41 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) {
 
-        ServerSocket serverSocket=new ServerSocket(3000);
-        System.out.println("Server started.. waiting for client...");
+        try{
+            ServerSocket serverSocket=new ServerSocket(3000);
+            System.out.println("Server started.. waiting for client...");
 
-        Socket socket= serverSocket.accept();
-        System.out.println("client connected");
+            Socket socket= serverSocket.accept();
+            System.out.println("client connected");
 
-        //Input and output setup
-        BufferedReader input=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter output= new PrintWriter(socket.getOutputStream(), true);
+            //Input and output setup
+            BufferedReader input=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output= new PrintWriter(socket.getOutputStream(), true);
 
-        //for typing in terminal
-        BufferedReader consoleInput= new BufferedReader(new InputStreamReader(System.in));
+            //for typing in terminal
+            BufferedReader consoleInput= new BufferedReader(new InputStreamReader(System.in));
 
-        String clientMessage, serverReply;
+            String clientMessage, serverReply;
 
-        while(true){
-        //Read from client
-            clientMessage= input.readLine();
-            if(clientMessage.equalsIgnoreCase("exit")) break;
-            System.out.println("client"+clientMessage);
+            while(true){
+                //Read from client
+                clientMessage= input.readLine();
+                if(clientMessage.equalsIgnoreCase("exit")) break;
+                System.out.println("client"+clientMessage);
 
-            //send reply
-            System.out.println("Server:");
-            serverReply= consoleInput.readLine();
-            output.println(serverReply);
+                //send reply
+                System.out.println("Server:");
+                serverReply= consoleInput.readLine();
+                output.println(serverReply);
 
-            if(serverReply.equalsIgnoreCase("exit")) break;
+                if(serverReply.equalsIgnoreCase("exit")) break;
+            }
+            socket.close();
+            serverSocket.close();
+            System.out.println("Connection closed...");
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-
+        }
     }
 }
