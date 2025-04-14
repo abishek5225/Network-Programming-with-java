fc8c4e05b71cd823b275819c6855d4721d8b8e4a
abishek5225 <abishekchaulagain5225@gmail.com>
2025-04-14 15:28:13 +0545

    updated

--- src/Internet-Address/InetAddressExample.java
+++ src/Internet-Address/InetAddressExample.java
@@ -3,13 +3,32 @@ import java.net.*;
 public class InetAddressExample{
     public static void main(String[] args) {
         try{
+            
+            InetAddress[] addresses = InetAddress.getAllByName("www.google.com");
+            for (InetAddress address : addresses) {
+                System.out.println("Host name: " + address.getHostName());
+                System.out.println("IP address: " + address.getHostAddress());
+                System.out.println("Canonical host name: " + address.getCanonicalHostName());
+                System.out.println();
+
+                System.out.println("Host name: " + address.toString());
+                System.out.println();
+            }
+            
+            /* 
             InetAddress address=InetAddress.getByName("8.8.8.8");
 
             System.out.println("Host name: "+ address.getHostName());
             System.out.println("IP address: "+ address.getHostAddress());
             System.out.println("canonical hostname: "+address.getCanonicalHostName());
 
+            //localhost info
+            InetAddress local=InetAddress.getLocalHost();
+            System.out.println("Local host name: "+ local.getHostName());
+            System.out.println("Local IP address: "+ local.getHostAddress());
+            System.out.println("Local canonical hostname: "+ local.getCanonicalHostName());
 
+            */
         } catch (java.lang.Exception e) {
             throw new RuntimeException(e);
         }
