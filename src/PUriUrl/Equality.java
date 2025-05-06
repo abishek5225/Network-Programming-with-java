2c0139a5c41b6f99d9e7d948ed58d59685af5e81
abishek5225 <abishekchaulagain5225@gmail.com>
2025-05-06 23:05:49 +0545

    updated

---
+++ src/PUriUrl/Equality.java
@@ -0,0 +1,19 @@
+package PUriUrl;
+
+import java.net.URL;
+
+public class Equality {
+    public static void main(String[] args) {
+        try{
+            URL url1=new URL("https://google..com");
+            URL url2=new URL("https://google..com");
+            if (url1.equals(url2)) {
+                System.out.println("Same Url");
+            }
+
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+        
+    }
+}
