2b43d8ad706e432eee9fde992891bcc29ebee04c
abishek5225 <abishekchaulagain5225@gmail.com>
2025-05-15 18:09:55 +0545

    updated

--- lookingHttp/client.java
+++ lookingHttp/client.java
@@ -1,7 +1,9 @@
 package lookingHttp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,11 +14,28 @@ public class client {
             System.out.println("Connected to server");
             
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            for(int i=1;i<=5;i++){
+                System.out.println("Sending request"+i);
+
+                 String request= "GET/HTTP/1.1\r\n"+
+                                    "Host: localhost\r\n"+
+                                    "Connection: keep-alive\r\n"+
+                                    "\r\n";
+
+                out.write(request);
+                out.flush();
+
+
+                //Read response
+                String line;
+                
+            }
             
             
         } catch (Exception e) {
            System.out.println(e);
         }
-        
     }
 }
