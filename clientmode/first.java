7c490520b319d49b3edb2d53d990f921b7fc1b2c
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-23 21:58:07 +0545

    updated

--- clientmode/first.java
+++ clientmode/first.java
@@ -1,11 +1,15 @@
 package clientmode;
 
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class first {
     public static void main(String[] args) {
         try{
             Socket socket=new Socket("localhost",3000);
+
+            PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+            out.println("Hello from client");
 
         }catch(Exception ex){
             System.out.println("Exception occured:" +ex);
