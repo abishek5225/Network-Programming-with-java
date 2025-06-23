0681cc7473e2c9d7b5cd0e82926c0980d6f21695
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-23 22:23:26 +0545

    updated

--- SecureSocket/second.java
+++ SecureSocket/second.java
@@ -1,7 +1,21 @@
 package SecureSocket;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
 public class second {
     public static void main(String[] args) {
-        
+        try{
+            int port= 3005;
+        System.setProperty("javax.net.ssl.keyStore", "serverkeystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+
+        SSLServerSocketFactory sslServerSocketFactory=(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
+        SSLServerSocket serverSocket=(SSLServerSocket) sslServerSocketFactory.createServerSocket(port);
+        System.out.println("");
+        }catch(Exception ex){
+            
+        }
     }
 }
