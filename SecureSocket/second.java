6fca1f0045d873009b1f89bb52bd8c2eb5b6d2f1
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-23 22:34:20 +0545

    updated

--- SecureSocket/second.java
+++ SecureSocket/second.java
@@ -1,8 +1,14 @@
 package SecureSocket;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+
+import lookingHttp.client;
 
 public class second {
     public static void main(String[] args) {
@@ -18,6 +24,14 @@ public class second {
 
         SSLSocket clientSocket=(SSLSocket) serverSocket.accept();
         System.out.println("Client connected");
+
+        BufferedReader in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out=new PrintWriter(clientSocket.getOutputStream(),true);
+
+        String message= in.readLine();
+        System.out.println("client says: " +message);
+        clientSocket.close();
+        serverSocket.close();
         }catch(Exception ex){
             System.out.println("Exception occured: "+ ex);
         }
