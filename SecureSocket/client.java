acb3ee0459c25855fe88877cf308bce29199fc40
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-23 20:27:48 +0545

    updated

--- SecureSocket/client.java
+++ SecureSocket/client.java
@@ -27,7 +27,13 @@ public class client {
         //read response
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        String line;
+        while((line=in.readLine()) != null){
+            System.out.println(line);
+        }
         
+
+
 
        }catch(Exception ex){
         System.out.println("Following exception occured: "+ex);
