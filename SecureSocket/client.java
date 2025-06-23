e312fb0ada47227d8077aefa10fe0cea9dfa5890
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-23 20:08:48 +0545

    updated

--- SecureSocket/client.java
+++ SecureSocket/client.java
@@ -1,7 +1,16 @@
 package SecureSocket;
 
+import javax.net.ssl.SSLSocketFactory;
+
 public class client {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+       String host= "www.google.com";
+       int port= 443;
+
+       try{
+        SSLSocketFactory Factory=(SSLSocketFactory)SSLSocketFactory.getDefault();
+       }catch(Exception ex){
+        System.out.println("Following exception occured: "+ex);
+       }
     }
 }
