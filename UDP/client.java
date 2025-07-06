2b68d2f79541466747898220c82155f389b9ff1e
abishek5225 <abishekchaulagain5225@gmail.com>
2025-07-06 21:43:25 +0545

    updated

--- UDP/client.java
+++ UDP/client.java
@@ -15,7 +15,15 @@ public class client{
         socket.send(packet);
 
         //receive response from server
+        byte[] receivebuffer=new byte[1024];
 
+        DatagramPacket receivePacket=new DatagramPacket(receivebuffer, port);
+        socket.receive(receivePacket);
+
+        String response= new String(receivePacket.getData(),0,receivePacket.getLength());
+
+        System.out.println("Server: "+response);
+        socket.close();
         
     }
 }
