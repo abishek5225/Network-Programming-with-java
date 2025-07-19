9357b3b730a807201a5de9efe8cc8e5a28d640e4
abishek5225 <abishekchaulagain5225@gmail.com>
2025-07-19 18:28:21 +0545

    updated

--- Uri/RetrivingData.java
+++ Uri/RetrivingData.java
@@ -2,3 +2,22 @@ package Uri;
 
 import java.net.*;
 import java.io.*;
+
+public class RetrivingData{
+    public static void main(String[] args) {
+       try{
+         URI uri=new URI("https://www.facebook.com/");
+         URL url=uri.toURL();
+
+        BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String line;
+        while((line=reader.readLine()) !=null){
+            System.out.println(line);
+        }
+        reader.close();
+       }catch(Exception ex){
+        System.out.println("Exception occured:"+ ex);
+       }
+    }
+}   
